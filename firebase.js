305de663c4f812a2f6cd01d41590ee817b330b0e
Vijay-SP <vijayprajapati6350@gmail.com>
2024-04-05 09:21:59 +0530

    firebase creds updated

--- firebase.js
+++ firebase.js
@@ -3,12 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-    apiKey: "AIzaSyDUQgIEBu_PhwWQn5bYWzW39rNJRO8eyK8",
-    authDomain: "pizza-d9b3e.firebaseapp.com",
-    projectId: "pizza-d9b3e",
-    storageBucket: "pizza-d9b3e.appspot.com",
-    messagingSenderId: "369038231274",
-    appId: "1:369038231274:web:8b8c35fa8341e00f583993"
+    apiKey: "AIzaSyCVlo8jRp5A3tkSV62wC5xJQr_3HAlwHCc",
+    authDomain: "bo-lend.firebaseapp.com",
+    projectId: "bo-lend",
+    storageBucket: "bo-lend.appspot.com",
+    messagingSenderId: "727138077389",
+    appId: "1:727138077389:web:130e5323da2cd44e23b784",
+    measurementId: "G-6P1ZKV95DF"
 };
 
 const app = initializeApp(firebaseConfig);
